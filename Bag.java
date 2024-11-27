0ae25152e5b70627341fe2bef8905d680c1c16a0
sarapresa <sarapresa@ua.pt>
2024-11-27 12:25:52 +0000

    Implementação e melhorias nas classes Class e Bag

--- Bag.java
+++ Bag.java
@@ -1,6 +1,61 @@
-Public class Bag {
+public class Bag {
+    // Atributos para armazenar informações sobre a mala
+    private int size; // Tamanho da mala (exemplo: dimensões aproximadas em cm)
+    private double weight; // Peso da mala (em quilogramas)
+    private int quantity; // Quantidade de malas associadas a um bilhete
+    private int ticketId; // Identificador do bilhete associado à mala
 
-    private int size;
-    private double weight;
-    private int quantity;
+    // Construtor para inicializar os atributos da mala
+    public Bag(int size, double weight, int quantity, int ticketId) {
+        this.size = size;
+        this.weight = weight;
+        this.quantity = quantity;
+        this.ticketId = ticketId;
+    }
+
+    // Métodos getters e setters para aceder e alterar os valores dos atributos
+
+    public int getSize() { 
+        return size; // Retorna o tamanho da mala
+    }
+
+    public void setSize(int size) { 
+        this.size = size; // Define ou altera o tamanho da mala
+    }
+
+    public double getWeight() { 
+        return weight; // Retorna o peso da mala
+    }
+
+    public void setWeight(double weight) { 
+        this.weight = weight; // Define ou altera o peso da mala
+    }
+
+    public int getQuantity() { 
+        return quantity; // Retorna a quantidade de malas
+    }
+
+    public void setQuantity(int quantity) { 
+        this.quantity = quantity; // Define ou altera a quantidade de malas
+    }
+
+    public int getTicketId() { 
+        return ticketId; // Retorna o identificador do bilhete associado
+    }
+
+    public void setTicketId(int ticketId) { 
+        this.ticketId = ticketId; // Define ou altera o identificador do bilhete
+    }
+
+    // Método para calcular custos adicionais caso o peso exceda um limite
+    public double calculateExtraCost(double weightLimit, double costPerKg) {
+        // Se o peso for maior que o limite permitido, calcula o custo adicional
+        return (weight > weightLimit) ? (weight - weightLimit) * costPerKg : 0;
+    }
+
+    // Método para exibir as informações da mala de forma legível
+    @Override
+    public String toString() {
+        return "Bag [Tamanho: " + size + ", Peso: " + weight + "kg, Quantidade: " + quantity + ", ID Bilhete: " + ticketId + "]";
+    }
 }
--- Class.java
+++ Class.java
@@ -1,8 +1,61 @@
 public class Class {
+    // Atributos para armazenar informações sobre a classe do voo
+    private String className; // Nome da classe (exemplo: "Primeira", "Económica")
+    private double price; // Preço associado à classe
+    private int seatCapacity; // Capacidade total de lugares nessa classe
+    private String services; // Descrição dos serviços disponíveis para a classe
 
-    private double price;
-    private Seat seat;
+    // Construtor para inicializar os atributos da classe
+    public Class(String className, double price, int seatCapacity, String services) {
+        this.className = className;
+        this.price = price;
+        this.seatCapacity = seatCapacity;
+        this.services = services;
+    }
 
+    // Métodos getters e setters para aceder e alterar os valores dos atributos
 
+    public String getClassName() {
+        return className; // Retorna o nome da classe
+    }
 
+    public void setClassName(String className) {
+        this.className = className; // Define ou altera o nome da classe
+    }
+
+    public double getPrice() {
+        return price; // Retorna o preço da classe
+    }
+
+    public void setPrice(double price) {
+        this.price = price; // Define ou altera o preço da classe
+    }
+
+    public int getSeatCapacity() {
+        return seatCapacity; // Retorna a capacidade total de lugares
+    }
+
+    public void setSeatCapacity(int seatCapacity) {
+        this.seatCapacity = seatCapacity; // Define ou altera a capacidade de lugares
+    }
+
+    public String getServices() {
+        return services; // Retorna os serviços disponíveis na classe
+    }
+
+    public void setServices(String services) {
+        this.services = services; // Define ou altera os serviços disponíveis
+    }
+
+    // Método para verificar se há lugares disponíveis
+    public boolean hasAvailableSeats(int bookedSeats) {
+        // Retorna verdadeiro se o número de lugares reservados for menor que a capacidade total
+        return bookedSeats < seatCapacity;
+    }
+
+    // Método para exibir as informações da classe de forma legível
+    @Override
+    public String toString() {
+        return "Class [Nome: " + className + ", Preço: " + price + ", Capacidade: " + seatCapacity + ", Serviços: " + services + "]";
+    }
 }
