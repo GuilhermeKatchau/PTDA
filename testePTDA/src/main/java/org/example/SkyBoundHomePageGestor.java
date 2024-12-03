46d1c67d27419a11399c385c2829eeccc7ebcf45
Loquitos13 <dbp13@ua.pt>
2024-12-03 16:10:43 +0000

    Interface 03/12 16:10

---
+++ testePTDA/src/main/java/org/example/SkyBoundHomePageGestor.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SkyBoundHomePageGestor {
+    private JPanel panel1;
+    private JButton ADICIONARVOOSButton;
+    private JButton CONSULTARVOOSButton;
+}
