a90a4d07375a1f203c44ab86d6016e94fe5a7b6d
GuilhermeKatchau <151835250+GuilhermeKatchau@users.noreply.github.com>
2024-12-03 21:17:11 +0000

    Update PTDAALELUHA.sql

--- PTDAALELUHA.sql
+++ PTDAALELUHA.sql
@@ -1,5 +1,5 @@
 USE PTDA24_BD_05;
-/*DROP TABLE IF EXISTS passenger,ticket,bag,airplane,flight,seat,service,checkin,crew,assistant,pilot;*/
+
 
 CREATE TABLE passenger (
 name_passenger VARCHAR(40),
@@ -62,7 +62,7 @@ id_Ticket INTEGER
 REFERENCES ticket(id),
 id_flight INTEGER
 REFERENCES flight(id),
-checkIn VARCHAR(40) PRIMARY KEY,
+checkIn VARCHAR(40) PRIMARY KEY,  -- deve ser booleano ?
 TimeCheckIn INTEGER
 ); 
 
@@ -76,12 +76,12 @@ ranq VARCHAR(20)
 
 CREATE TABLE assistant (
     id_assistant INTEGER PRIMARY KEY,
-    FOREIGN KEY (id_assistant) REFERENCES crew(id)
+    FOREIGN KEY (id_assistant) REFERENCES crew(id) -- id_crew REFERENCES crew(id) ?
 );
 
 CREATE TABLE pilot (
     id_pilot INTEGER PRIMARY KEY,
-    FOREIGN KEY (id_pilot) REFERENCES crew(id)
+    FOREIGN KEY (id_pilot) REFERENCES crew(id) -- id_crew REFERENCES crew(id) ?
 );
 
 
@@ -93,7 +93,7 @@ IN source1 VARCHAR(40)
 )
 BEGIN 
 INSERT INTO airplane(id,destination,source1)
-VALUES (id_plane,destination_airplane,source1_airplane);
+VALUES (id,destination,source1); 
 END //
 DELIMITER ;
 
@@ -110,7 +110,7 @@ IN codename VARCHAR(40)
 )
 BEGIN 
 INSERT INTO flight(id_plane,id,maxPassengers,timeTakeOff,timeLanding,destination,source1,codename)
-VALUES (id_plane_flight,id_flight,maxPassengers_flight,timeTakeOff_flight,timeLanding_flight,destination_flight,source1_flight,codename_flight);
+VALUES (id_plane_flight,id_flight,maxPassengers_flight,timeTakeOff_flight,timeLanding_flight,destination,source1,codename);
 END //
 DELIMITER ;
 DELIMITER //
@@ -119,8 +119,7 @@ FOR EACH ROW
 BEGIN 
 IF NOT EXISTS (
     SELECT 1 FROM flight WHERE id_plane = NEW.id_plane) THEN 
-(
-SET NEW.id_plane =(SELECT id FROM airplane WHERE id NOT IN (SELECT id_plane FROM flight )
+(SET NEW.id_plane =(SELECT id FROM airplane WHERE id NOT IN (SELECT id_plane FROM flight )
 LIMIT 1)
 );
 END IF;
@@ -130,6 +129,7 @@ SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No planes available';
 END IF;
 END
 DELIMITER;
+
 
 ALTER TABLE ticket
 ADD COLUMN id_flight INTEGER REFERENCES flight(id);
@@ -148,19 +148,18 @@ SELECT id  INTO flight_id
     IF id_flight IS NOT NULL THEN 
     UPDATE ticket 
     SET id_flight = flight_id
-    WHERE id = NEW.id
-		ELSE
+    WHERE id = NEW.id;
+	ELSE
 	SIGNAL SQLSTATE '45000' 
         SET MESSAGE_TEXT = 'No matching flight found for the ticket.';
     END IF;
 END //
 
 DELIMITER ;
-    
+
 
 
 SELECT * FROM flight;
 SELECT * FROM airplane;
 SELECT * FROM passenger;
 SELECT * FROM ticket;
-
