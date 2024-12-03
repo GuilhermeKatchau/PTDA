e157e89e2abd47cbb585fca8416f76f007434494
unknown <brunolopesjardim@ua.pt>
2024-12-03 22:10:26 +0000

    Interface a Sequela 3/12 22:10

---
+++ testePTDA/src/main/java/org/example/SkyBoundAdicionarAviao.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SkyBoundAdicionarAviao {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
