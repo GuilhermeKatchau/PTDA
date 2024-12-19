cde3421e38e46d68af9a843937547295d57cf878
GuilhermeKatchau <151835250+GuilhermeKatchau@users.noreply.github.com>
2024-12-19 17:44:50 +0000

    HOMEPAGE USER

--- testePTDA/src/main/java/org/example/SkyBoundHomePage.java
+++ testePTDA/src/main/java/org/example/SkyBoundHomePage.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SkyBoundHomePage {
 
@@ -8,4 +10,27 @@ public class SkyBoundHomePage {
     private JButton inícioButton;
     private JTextArea skyBoundTextArea;
     private JPanel panel2;
+
+    public SkyBoundHomePage() {
+        inícioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Quando o botão for clicado, cria uma nova instância de CompraBilhete
+                new CompraBilhete();
+                // Fecha a janela atual
+                JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(panel1);
+                if (topFrame != null) {
+                    topFrame.dispose();
+                }
+            }
+        });
+    }
+public static void main(String[] args) {
+    // Criar a janela e mostrá-la
+    JFrame frame = new JFrame("SkyBound HomePage");
+    frame.setContentPane(new SkyBoundHomePage().panel1);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+}
 }
