f03c9cf31496eeb961fa1accd03f24269e25d034
unknown <brunolopesjardim@ua.pt>
2024-12-20 17:35:24 +0000

    adicionarAssentos modificações 20/12 17:35

--- testePTDA/src/main/java/org/example/SkyBoundAdicionarAssento.java
+++ testePTDA/src/main/java/org/example/SkyBoundAdicionarAssento.java
@@ -79,14 +79,14 @@ public class SkyBoundAdicionarAssento extends JFrame {
     private void processarEscolhaAssento( JButton botao, int numeroAssento) {
         botao.setBackground(Color.orange);
         JOptionPane.showMessageDialog(this, "Você escolheu o assento: " + numeroAssento);
-        enviarInformacoesAssento(botao, numeroAssento);
+        enviarInformacoesAssento(numeroAssento);
     }
 
 
-    private Object[] enviarInformacoesAssento(JButton botao, int numeroAssento) {
+    private int enviarInformacoesAssento( int numeroAssento) {
         // Lógica para enviar as informações do botão e número do assento
         System.out.println("Assento selecionado: " + numeroAssento);
-        return new Object[]{botao, numeroAssento};
+        return numeroAssento;
     }
     private void saveSeat(int idTicket, int idSeat, double price,Class classe){
         Main.saveSeatInfo(idTicket, idSeat, price,classe);
